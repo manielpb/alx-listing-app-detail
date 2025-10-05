4809f298cbf5a34102876669458b9e52af0588ee
maniel <manuelpepr99@icloud.com>
2025-10-05 22:13:38 +0000

    Implement Responsive Property Detail Page with Tailwind CSS and Dynamic Components

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,14 +1,11 @@
+import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
 import { useRouter } from "next/router";
-import { PROPERTYLISTINGSAMPLE } from "@/constants";
 import PropertyDetail from "@/components/property/PropertyDetail";
 
 export default function PropertyPage() {
   const router = useRouter();
   const { id } = router.query;
-
-  const property = PROPERTYLISTINGSAMPLE.find(
-    (item) => item.name === id
-  );
+  const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
 
   if (!property) return <p>Property not found</p>;
 
